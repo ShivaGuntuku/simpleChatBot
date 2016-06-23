9a901fbfac299a845ff32d883986d8bcf1ef05e5
ShivaPrasad <guntuku.shiva@gmail.com>
2016-06-23 15:25:35 +0530

    Create brain.py
    used for loading the aiml files for subsequent loading

---
+++ brain.py
@@ -0,0 +1,15 @@
+#this file speedup the loading AIML file
+import aiml
+import os
+
+kernel = aiml.Kernel()
+
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
+    kernel.saveBrain("bot_brain.brn")
+
+# kernel now ready for use
+while True:
+    print kernel.respond(raw_input("Enter your message >> "))
